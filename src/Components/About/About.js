26d02bb01158761bab335d7ffb5a1d0048469a0d
Tabrez Ansari <tabrezsvce@gmail.com>
2019-08-16 20:49:09 +0530

    new styles added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -8,11 +8,18 @@ export default class About extends Component{
         return(
                <div className="aboutContainer">
                 <div className="container">
+                <span className="htmlOpen"> &nbsp;&nbsp;&nbsp;&lt;html&gt;</span>
+                 <span className="bodyOpen"> &nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
                     <div className="devAbout">
+                    <span className="h1Open"> &nbsp;&nbsp;&nbsp;&lt;h1&gt;</span>
                         <h3 className="aboutHead">
                             About Me 
                         </h3>
+                        <span className="h1Close"> &nbsp;&nbsp;&nbsp;&lt;/h1&gt;</span>
+
                         <div className="info">
+                        <span className="pOpen"> &nbsp;&nbsp;&nbsp;&lt;p&gt;</span>
+
                             <div className="leftDesc">
                              <p className="brDesc">
                              Hello! I'm Tabrez Ansari, a software Developer based in Bangalore, BE who enjoys building things that live on the internet or for the desktop. I develop exceptional desktop,mobile,and web apps that provide intuitive, pixel-perfect user interfaces with efficient and modern backends.
@@ -22,10 +29,14 @@ export default class About extends Component{
                             Shortly after graduating from Vishveshwaraya Technological University, I joined the engineering team at Intellicar Telematics Pvt. Ltd. where I work on a wide variety of interesting and meaningful projects on a daily basis.
                             </p>
                             </div>
+                            <span className="pClose"> &nbsp;&nbsp;&nbsp;&lt;/p&gt;</span>
+
                             <div className="rightDesc">
                                 <Skills/>
                             </div>
                         </div>
+                        <span className="bodyClose"> &nbsp;&nbsp;&nbsp;&lt;/body&gt;</span>
+                  <span className="htmlClose"> &nbsp;&nbsp;&nbsp;&lt;/html&gt;</span>
                         
                     </div>
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -14,26 +14,26 @@ export default class Home extends Component{
                         <span className="pOpen"> &nbsp;&nbsp;&nbsp;&lt;p&gt;</span>
                             <h1 className="devHi">
                             Hi, my name is</h1>
-                        <span className="pClose"> &nbsp;&nbsp;&nbsp;&lt;p/&gt;</span>
+                        <span className="pClose"> &nbsp;&nbsp;&nbsp;&lt;/p&gt;</span>
 
                         <span className="h1Open"> &nbsp;&nbsp;&nbsp;&lt;h1&gt;</span>
                             <h1 className="devName">Tabrez Ansari</h1>                      
 
                             <h1 className="devTag">I build things for web</h1>
-                         <span className="h1Close"> &nbsp;&nbsp;&nbsp;&lt;h1/&gt;</span>
+                         <span className="h1Close"> &nbsp;&nbsp;&nbsp;&lt;/h1&gt;</span>
 
                         <span className="p2Open"> &nbsp;&nbsp;&nbsp;&lt;p&gt;</span>  
                             <div className="devDesc">
                                 <p>I'm a software Developer based in Bangalore, B.E specializing in building exceptional, high-quality websites and applications.</p>
                             </div>
-                        <span className="p2Close"> &nbsp;&nbsp;&nbsp;&lt;p/&gt;</span>
+                        <span className="p2Close"> &nbsp;&nbsp;&nbsp;&lt;/p&gt;</span>
 
                         <span className="btnOpen"> &nbsp;&nbsp;&nbsp;&lt;btn&gt;</span>
                           <button className="moreBtn">Read More </button>
-                         <span className="btnClose"> &nbsp;&nbsp;&nbsp;&lt;btn/&gt;</span>
+                         <span className="btnClose"> &nbsp;&nbsp;&nbsp;&lt;/btn&gt;</span>
                   </div>
-                  <span className="bodyClose"> &nbsp;&nbsp;&nbsp;&lt;body/&gt;</span>
-                  <span className="htmlClose"> &nbsp;&nbsp;&nbsp;&lt;html/&gt;</span>
+                  <span className="bodyClose"> &nbsp;&nbsp;&nbsp;&lt;/body&gt;</span>
+                  <span className="htmlClose"> &nbsp;&nbsp;&nbsp;&lt;/html&gt;</span>
 
                </div>
 
