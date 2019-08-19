d71b3e3362cccad88bebf5e6af4f2bcb23890703
Tabrez Ansari <tabrezsvce@gmail.com>
2019-08-19 13:47:47 +0530

    new design added for skills

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import './About.css'
 import Skills from '../Skills/Skills'
 import $ from 'jquery'
+import react from '../../Assets/skills/react.svg'
+import {tools,lang} from '../Constants/constants';
 export default class About extends Component{
 
     render(){
@@ -11,6 +13,7 @@ export default class About extends Component{
                 <span className="htmlOpen"> &nbsp;&nbsp;&nbsp;&lt;html&gt;</span>
                  <span className="bodyOpen"> &nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
                     <div className="devAbout">
+                        <div className="leftSide">
                     <span className="h1Open"> &nbsp;&nbsp;&nbsp;&lt;h1&gt;</span>
                         <h3 className="aboutHead">
                             About Me 
@@ -31,12 +34,50 @@ export default class About extends Component{
                             </div>
                             <span className="pClose"> &nbsp;&nbsp;&nbsp;&lt;/p&gt;</span>
 
-                            <div className="rightDesc">
-                                <Skills/>
-                            </div>
+                          
                         </div>
                         <span className="bodyClose"> &nbsp;&nbsp;&nbsp;&lt;/body&gt;</span>
-                  <span className="htmlClose"> &nbsp;&nbsp;&nbsp;&lt;/html&gt;</span>
+                         <span className="htmlClose"> &nbsp;&nbsp;&nbsp;&lt;/html&gt;</span>
+                    </div>
+                    <div className="rightSide">
+                                <div className="tooltech">
+                                <h3 className="toolHead">
+                                    Tools &amp; Frameworks 
+                                </h3>
+                                <div className="skillsCont">
+                                 {
+                                    tools.map((data)=>(
+                                        <div className="skill">
+                                        <div className="skillImg">
+                                            <img src={require("../../Assets/skills/"+data.logo)} />
+                                        </div>
+                                        <span>{data.name}</span>
+                                    </div>
+                                    ))
+                                 }
+                                   
+                                </div>
+                                </div>
+
+                                <div className="tooltech">
+                                <h3 className="toolHead">
+                                    Programming Languages 
+                                </h3>
+                                <div className="skillsCont">
+                                 {
+                                    lang.map((data)=>(
+                                        <div className="skill">
+                                        <div className="skillImg">
+                                            <img src={require("../../Assets/skills/"+data.logo)} />
+                                        </div>
+                                        <span>{data.name}</span>
+                                    </div>
+                                    ))
+                                 }
+                                   
+                                </div>
+                                </div>
+                            </div>
                         
                     </div>
 
---
+++ src/Components/Constants/constants.js
@@ -0,0 +1,76 @@
+export const  tools=[
+    {   "id":1,
+        "name":"React",
+        "logo":'react.svg'
+    },
+    {   "id":2,
+        "name":"Docker",
+        "logo":'docker.svg'
+    },
+    {   "id":3,
+        "name":"Angular",
+        "logo":'angular.svg'
+    },
+    {   "id":4,
+        "name":"Jquery",
+        "logo":'jquery.svg'
+    },
+    {   "id":5,
+        "name":"Apache",
+        "logo":'apache.svg'
+    },
+    {   "id":6,
+        "name":"Git",
+        "logo":'git.svg'
+    },
+    {   "id":7,
+        "name":"Amazon AWS",
+        "logo":'aws.svg'
+    },
+    {   "id":8,
+        "name":"Laravel",
+        "logo":'laravel.svg'
+    },
+    {   "id":9,
+        "name":"Firebase",
+        "logo":'firebase.svg'
+    },
+    {   "id":10,
+        "name":"Webpack",
+        "logo":'webpack.svg'
+    },
+    
+]
+
+
+export const  lang=[
+    {   "id":1,
+        "name":"Javascript",
+        "logo":'js.svg'
+    },
+    {   "id":2,
+        "name":"PHP",
+        "logo":'php.svg'
+    },
+    {   "id":3,
+        "name":"MySql",
+        "logo":'mysql.svg'
+    },
+    {   "id":4,
+        "name":"Sass",
+        "logo":'sass.svg'
+    },
+    {   "id":5,
+        "name":"CSS",
+        "logo":'css.svg'
+    },
+    {   "id":6,
+        "name":"HTML",
+        "logo":'html.svg'
+    }
+    
+]
+
+
+
+export default {tools,lang};
