f124c423c1a4bf52971a0c524f33a0aed236497d
Tabrez Ansari <tabrezsvce@gmail.com>
2019-12-10 12:44:42 +0530

    seo optimization

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,96 +1,81 @@
-import React, { Component } from 'react';
-import './About.css'
-import Skills from '../Skills/Skills'
-import $ from 'jquery'
-import react from '../../Assets/skills/react.svg'
-import { tools, lang } from '../Constants/constants';
+import React, { Component } from "react";
+import "./About.css";
+import Skills from "../Skills/Skills";
+import $ from "jquery";
+import react from "../../Assets/skills/react.svg";
+import { tools, lang } from "../Constants/constants";
 export default class About extends Component {
+  render() {
+    return (
+      <div className="aboutContainer">
+        <div className="container">
+          <div className="top-tags">
+            <span className="htmlOpen"> &nbsp;&nbsp;&nbsp;&lt;html&gt;</span>
+            <span className="bodyOpen"> &nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
+          </div>
+          <div className="devAbout">
+            <div className="leftSide">
+              <h3 className="aboutHead">About Me</h3>
 
-    render() {
-        return (
-            <div className="aboutContainer">
-                <div className="container">
-                  <div className="top-tags">
-                        <span className="htmlOpen"> &nbsp;&nbsp;&nbsp;&lt;html&gt;</span>
-                        <span className="bodyOpen"> &nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
-                    </div>
-                    <div className="devAbout">
-                        <div className="leftSide">
-                            <h3 className="aboutHead">
-                                 About Me
-                              </h3>
-
-                            <div className="info">
-
-                                <div className="leftDesc">
-                                    <p className="brDesc">
-                                        Hello! I'm Tabrez Ansari, a software Developer based in Bangalore, BE who enjoys building things  for Web ,Mobile and desktop. I develop exceptional desktop,mobile,and web apps .
-           
-                            </p>
-                                    <p className="brDesc">
-                                        Shortly after graduating from Vishveshwaraya Technological University, I joined the engineering team at Intellicar Telematics Pvt. Ltd. where I work on a wide variety of interesting and meaningful projects on a daily basis.
-                            </p>
-                                </div>
-
-
-                            </div>
-                          
-                        </div>
-                        <div className="rightSide">
-                            <div className="tooltech">
-                                <h3 className="toolHead">
-                                    Tools &amp; Frameworks
-                                </h3>
-                                <div className="skillsCont">
-                                    {
-                                        tools.map((data) => (
-                                            <div className="skill">
-                                                <div className="skillImg">
-                                                    <img src={require("../../Assets/skills/" + data.logo)} />
-                                                </div>
-                                                <span>{data.name}</span>
-                                            </div>
-                                        ))
-                                    }
-
-                                </div>
-                            </div>
-
-                            <div className="tooltech">
-                                <h3 className="toolHead">
-                                    Programming Languages
-                                </h3>
-                                <div className="skillsCont">
-                                    {
-                                        lang.map((data) => (
-                                            <div className="skill">
-                                                <div className="skillImg">
-                                                    <img src={require("../../Assets/skills/" + data.logo)} />
-                                                </div>
-                                                <span>{data.name}</span>
-                                            </div>
-                                        ))
-                                    }
-
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-
-                    <div className="bottom-tags">
-                        <span className="bodyClose"> &nbsp;&nbsp;&nbsp;&lt;/body&gt;</span>
-                        <span className="htmlClose"> &nbsp;&nbsp;&nbsp;&lt;/html&gt;</span>
-                 </div>
+              <div className="info">
+                <div className="leftDesc">
+                  <p className="brDesc">
+                    Hello! I'm Tabrez Ansari, a software Developer based in
+                    Bangalore, BE who enjoys building things for Web ,Mobile and
+                    desktop. I develop exceptional desktop,mobile,and web apps .
+                  </p>
+                  <p className="brDesc">
+                    Shortly after graduating from Vishveshwaraya Technological
+                    University, I joined the engineering team at Intellicar
+                    Telematics Pvt. Ltd. where I work on a wide variety of
+                    interesting and meaningful projects on a daily basis.
+                  </p>
                 </div>
-                
-                    
-
-
-
+              </div>
             </div>
+            <div className="rightSide">
+              <div className="tooltech">
+                <h3 className="toolHead">Tools &amp; Frameworks</h3>
+                <div className="skillsCont">
+                  {tools.map(data => (
+                    <div className="skill">
+                      <div className="skillImg">
+                        <img
+                          src={require("../../Assets/skills/" + data.logo)}
+                          alt="logo"
+                        />
+                      </div>
+                      <span>{data.name}</span>
+                    </div>
+                  ))}
+                </div>
+              </div>
 
-        )
-    }
+              <div className="tooltech">
+                <h3 className="toolHead">Programming Languages</h3>
+                <div className="skillsCont">
+                  {lang.map(data => (
+                    <div className="skill">
+                      <div className="skillImg">
+                        <img
+                          src={require("../../Assets/skills/" + data.logo)}
+                          alt="logo"
+                        />
+                      </div>
+                      <span>{data.name}</span>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="bottom-tags">
+            <span className="bodyClose"> &nbsp;&nbsp;&nbsp;&lt;/body&gt;</span>
+            <span className="htmlClose"> &nbsp;&nbsp;&nbsp;&lt;/html&gt;</span>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,65 +1,95 @@
-import React,{Component} from 'react';
-import './Header.css';
-import Resume from '../../Assets/tabrezansari.pdf'
-import tabrez from '../../Assets/tabrez.png'
-import tabrezsvg from '../../Assets/tabrezsvg.svg'
-import { normalize } from 'path';
-export default class Header extends Component{
+import React, { Component } from "react";
+import "./Header.css";
+import Resume from "../../Assets/TabrezAnsari.pdf";
+import tabrez from "../../Assets/tabrez.png";
+import tabrezsvg from "../../Assets/tabrezsvg.svg";
+import { normalize } from "path";
+export default class Header extends Component {
+  setViewIndex = index => {
+    this.props.viewHandler(index);
+  };
+  showNav = () => {
+    let mobNav = document.getElementById("head-mobile");
+    mobNav.style.display = "block";
+  };
 
-setViewIndex=(index)=>{
-this.props.viewHandler(index);
-}
-showNav=()=>{
-    let mobNav=document.getElementById("head-mobile");
-    mobNav.style.display="block";
-}
+  render() {
+    return (
+      <div className="header">
+        <img className="imgLogo" src={tabrez} alt="MainLogo" />
+        <div className="menu-bar" onClick={() => this.showNav()}>
+          <i
+            style={{ fontStyle: "normal" }}
+            className="fa fa-bars menu-mobile"
+          ></i>
+        </div>
+        <div id="head-mobile" className="header-mobile">
+          <div className="menus">
+            <a className="menus-a" onClick={() => this.setViewIndex(0)}>
+              <i className="fa fa-home header-icon"></i>
+            </a>
+            <a className="menus-a" onClick={() => this.setViewIndex(1)}>
+              <i className="fa fa-user header-icon"></i>{" "}
+            </a>
+            <a className="menus-a" onClick={() => this.setViewIndex(2)}>
+              <i className="fa fa-briefcase  header-icon"></i>{" "}
+            </a>
+            <a className="menus-a" onClick={() => this.setViewIndex(3)}>
+              <i className="fa fa-envelope header-icon"></i>{" "}
+            </a>
+          </div>
 
-    render(){
-        return(
-            <div className="header">
-                <img className="imgLogo" src={tabrez} />
-                <div className="menu-bar" onClick={()=>this.showNav()}>
-                 <i style={{fontStyle:'normal'}} className="fa fa-bars menu-mobile"></i>
-                </div>
-                <div id="head-mobile" className="header-mobile">
-                    <div className="menus">
-                    <a className="menus-a" onClick={()=>this.setViewIndex(0)}><i className="fa fa-home header-icon"></i></a>
-                    <a className="menus-a"  onClick={()=>this.setViewIndex(1)}><i className="fa fa-user header-icon"></i> </a>
-                    <a className="menus-a" onClick={()=>this.setViewIndex(2)}><i className="fa fa-briefcase  header-icon"></i> </a>
-                    <a className="menus-a" onClick={()=>this.setViewIndex(3)}><i className="fa fa-envelope header-icon"></i> </a>
-                    </div>
-
-                    <div className="social-mobile">
-                    <ul>
-                        <li>
-                            <a href="https://www.facebook.com/tabrezprofile" target="_blank"><i className="fa fa-facebook-square"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://github.com/tabrezansari" target="_blank"> <i className="fa fa-github"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://www.linkedin.com/in/tabrezprofile/" target="_blank"> <i className="fa fa-linkedin"></i></a>
-                        </li>
-                        <li>
-                            <a href="https://twitter.com/tabrezansari_" target="_blank"><i className="fa fa-twitter"></i></a>
-                        </li>
-
-                    </ul>
-
-                </div>
-
-
-                </div>
-                <div className="header-right">
-                    <a  onClick={()=>this.setViewIndex(0)}><small>01.</small> Home</a>
-                    <a   onClick={()=>this.setViewIndex(1)}><small>02.</small> About</a>
-                    <a  onClick={()=>this.setViewIndex(2)}><small>03.</small> Experience</a>
-                    <a  onClick={()=>this.setViewIndex(3)}><small>04.</small> Contact</a>
-                    <a href={Resume} target="_blank" className="resumeBtn">RESUME</a>
-
-
-                </div>
-            </div>
-        )
-    }
+          <div className="social-mobile">
+            <ul>
+              <li>
+                <a
+                  href="https://www.facebook.com/tabrezprofile"
+                  target="_blank"
+                >
+                  <i className="fa fa-facebook-square"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/tabrezansari" target="_blank">
+                  {" "}
+                  <i className="fa fa-github"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/tabrezprofile/"
+                  target="_blank"
+                >
+                  {" "}
+                  <i className="fa fa-linkedin"></i>
+                </a>
+              </li>
+              <li>
+                <a href="https://twitter.com/tabrezansari_" target="_blank">
+                  <i className="fa fa-twitter"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="header-right">
+          <a onClick={() => this.setViewIndex(0)}>
+            <small>01.</small> Home
+          </a>
+          <a onClick={() => this.setViewIndex(1)}>
+            <small>02.</small> About
+          </a>
+          <a onClick={() => this.setViewIndex(2)}>
+            <small>03.</small> Experience
+          </a>
+          <a onClick={() => this.setViewIndex(3)}>
+            <small>04.</small> Contact
+          </a>
+          <a href={Resume} target="_blank" className="resumeBtn">
+            RESUME
+          </a>
+        </div>
+      </div>
+    );
+  }
 }
